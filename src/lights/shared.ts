04fa1737a71c62cc621c311b64e2f238d3ad9eba
DranoTheCat <47464311+DranoTheCat@users.noreply.github.com>
2023-01-25 13:53:32 -0800

    fds

--- src/lights/shared.ts
+++ src/lights/shared.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-inferrable-types */
 export const combine: string = `vec3 intensity = diffuse * attenuation;
 vec4 diffuseColor = texture2D(uSampler, texCoord);
-//vec3 finalColor = diffuseColor.rgb * intensity;
 vec3 finalColor = diffuseColor.rgb;
 if (testColor.rgb != normalColor.rgb) {
     finalColor = diffuseColor.rgb * intensity * airColor.a;
-    gl_FragColor = vec4(finalColor, diffuseColor.a);
+    gl_FragColor = vec4(finalColor, 1.0);
+    //gl_FragColor = vec4(finalColor, diffuseColor.a);
 } else {
     gl_FragColor = vec4(finalColor, 1.0);
 }
