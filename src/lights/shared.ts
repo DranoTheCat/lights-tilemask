d386826a5b721685c05e533f8fe30c3d9ed6790f
DranoTheCat <47464311+DranoTheCat@users.noreply.github.com>
2023-01-25 14:38:29 -0800

    ss

--- src/lights/shared.ts
+++ src/lights/shared.ts
@@ -7,6 +7,7 @@ if (testColor.rgb != normalColor.rgb) {
     //gl_FragColor = vec4(finalColor, 1.0);
     gl_FragColor = vec4(finalColor, airColor.a);
 } else {
+    finalColor = vec3(0, 1, 1);
     gl_FragColor = vec4(finalColor, diffuseColor.a);
 }
 
