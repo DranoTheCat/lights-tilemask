429dd32f036c2a4a8abca00db1c270d4818cfecd
DranoTheCat <47464311+DranoTheCat@users.noreply.github.com>
2023-01-26 12:16:41 -0800

    fds

--- src/lights/shared.ts
+++ src/lights/shared.ts
@@ -2,7 +2,8 @@
 export const combine: string = `vec3 intensity = diffuse * attenuation;
 vec4 diffuseColor = texture2D(uSampler, texCoord);
 if (testColor.r == normalColor.r && testColor.g == normalColor.g && testColor.b == normalColor.b) {
-    gl_FragColor = skyColor;
+    gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
+    //gl_FragColor = skyColor;
     //gl_FragColor = vec4(diffuseColor.rgb, diffuseColor.a);
     //gl_FragColor = diffuseColor;
     //gl_FragColor = vec4(diffuseColor.r, diffuseColor.g, diffuseColor.b, diffuseColor.a);
