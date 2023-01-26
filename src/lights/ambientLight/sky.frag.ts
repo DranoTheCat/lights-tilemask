d0fcde4f61994e5762e932720e3004c743b7aa63
DranoTheCat <47464311+DranoTheCat@users.noreply.github.com>
2023-01-26 13:17:14 -0800

    bn

--- src/lights/ambientLight/sky.frag.ts
+++ src/lights/ambientLight/sky.frag.ts
@@ -10,7 +10,8 @@ ${computeVertexPosition}
 
 vec4 normalColor = texture2D(uNormalSampler, texCoord);
 
-if (normalColor.a == 0.0) {
+// dfeault normal is 7f7fff == vec3(0.498,0.498,1.0)
+if (normalColor.a == 0.0 || (normalColor.r == 0.498 && normalColor.g == 0.498 && normalColor.b == 1.0)) {
     vec4 skyColor = texture2D(uSkySampler, texCoord);
     gl_FragColor = skyColor;
 } else {
