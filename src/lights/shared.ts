2a05f0d9d2b62a2b296688f05cefea5acca2f82d
DranoTheCat <47464311+DranoTheCat@users.noreply.github.com>
2023-01-25 17:24:31 -0800

    nc

--- src/lights/shared.ts
+++ src/lights/shared.ts
@@ -4,8 +4,8 @@ vec4 diffuseColor = texture2D(uSampler, texCoord);
 vec3 finalColor = diffuseColor.rgb;
 if (testColor.rgb != normalColor.rgb) {
     finalColor = diffuseColor.rgb * intensity * airColor.a;
-    //gl_FragColor = vec4(normalColor.rgb, 1.0);
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    gl_FragColor = vec4(normalColor.rgb, 1.0);
+    //gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
     //gl_FragColor = vec4(finalColor, airColor.a);
 } else {
     finalColor = vec3(0.0, 0.0, 1.0);
