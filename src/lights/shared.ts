aceca1b7c9a69ac72633db42c9fd62505f55f83b
DranoTheCat <47464311+DranoTheCat@users.noreply.github.com>
2023-01-25 19:21:13 -0800

    diffuseColor

--- src/lights/shared.ts
+++ src/lights/shared.ts
@@ -9,7 +9,7 @@ if (testColor.r == normalColor.r && testColor.g == normalColor.g && testColor.b 
     //gl_FragColor = vec4(normalColor.rgb, 1.0);
     //gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
     //gl_FragColor = vec4(finalColor.rgb, airColor.a);
-    gl_FragColor = vec4(diffuseColor.rgb, diffuseColor.a);
+    gl_FragColor = vec4(diffuseColor.r, diffuseColor.g. diffuseColor.b, diffuseColor.a);
 }
 `;
 
