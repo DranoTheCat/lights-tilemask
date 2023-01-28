afa98ce10c8141b721164481c6abcea9f76fe2b9
DranoTheCat <47464311+DranoTheCat@users.noreply.github.com>
2023-01-28 02:16:50 -0800

    s

--- src/lights/ambientLight/sky.frag.ts
+++ src/lights/ambientLight/sky.frag.ts
@@ -14,14 +14,11 @@ vec4 fowColor = texture2D(uFowSampler, texCoord);
 if (diffuseColor.a == 0.0) {
     vec4 skyColor = texture2D(uSkySampler, texCoord);
     vec3 finalColor = skyColor.rgb * uColor.rgb;
-    vec4 finalFinalColor = vec4(finalColor.rgb, skyColor.a) * fowColor;
-    gl_FragColor = finalFinalColor;
-
+    gl_FragColor = vec4(finalColor, skyColor.a) * fowColor;
 } else {
     // simplified lambert shading that makes assumptions for ambient color
     vec3 diffuse = uColor.rgb * uBrightness;
     vec3 finalColor = diffuseColor.rgb * diffuse;
-
     gl_FragColor = vec4(finalColor, diffuseColor.a);
 }
 
