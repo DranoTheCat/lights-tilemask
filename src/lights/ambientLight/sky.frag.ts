5cf41b421b403d41798ab218ca17ca0001c4e6b0
DranoTheCat <47464311+DranoTheCat@users.noreply.github.com>
2023-01-28 02:47:09 -0800

    fds

--- src/lights/ambientLight/sky.frag.ts
+++ src/lights/ambientLight/sky.frag.ts
@@ -19,7 +19,7 @@ if (diffuseColor.a == 0.0) {
     // simplified lambert shading that makes assumptions for ambient color
     vec3 diffuse = uColor.rgb * uBrightness;
     vec3 finalColor = diffuseColor.rgb * diffuse;
-    gl_FragColor = vec4(finalColor, diffuseColor.a) * fowColor;
+    gl_FragColor = vec4(finalColor, diffuseColor.a);// * fowColor;
 }
 
 }
